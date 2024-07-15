4ef71116002cd4e6c91a0e04a87cdaa0ca616b87
hypeedev <hypeedev@users.noreply.github.com>
2024-07-15 02:38:44 +0200

    Remove unused struct.

--- src/post_body.rs
+++ src/post_body.rs
@@ -1,12 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct Struct {
-    #[serde(rename = "sortCategory")]
-    pub sort_category: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct RangeYear {
     pub min: String,
     pub max: String,
